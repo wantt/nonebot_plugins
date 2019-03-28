43eacb25c285bad29276d2e24b8ccb51b783f970
wantt <wantt2@qq.com>
2019-03-28 08:46:40 +0000

    del file

--- auesome-bot/config.py
+++
@@ -1,7 +0,0 @@
-from nonebot.default_config import *
-
-SUPERUSERS = {1048517841,2188156040}
-COMMAND_START = {'', '/', '!', '／', '！'}
-
-HOST = '0.0.0.0'
-PORT = 8080
--- auesome-bot/test.py
+++
@@ -1,6 +0,0 @@
-with open('./adgroup.txt','r') as f:
-    for id in f.read().split('\n')[:-1]:
-        print(id)
-
-with open('./adgroup.txt','a+') as f:
-    f.write('1\n')
